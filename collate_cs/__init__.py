3ed8624c85e71d2029a603e8dce7772dc64b51ef
cogmob <luke.avery@live.co.uk>
2018-01-12 11:14:37 +0000

    do not create source files for empty directories

--- collate_cs/__init__.py
+++ collate_cs/__init__.py
@@ -9,7 +9,7 @@ if len(sys.argv) < 4:
 collate_cs._(sys.argv[1], sys.argv[2], int(sys.argv[3]), sys.argv[4])
 
 
-__version__ = "0.44"
+__version__ = "0.45"
 
 __title__ = "collate_cs"
 __description__ = "thin -> cs"
--- collate_cs/get_src.py
+++ collate_cs/get_src.py
@@ -1,12 +1,13 @@
 import os
 import read_class_files as read
 import process_class_files as process
+import cs_file_count
 
 def _(path, namespace_depth, prefix):
     top = {'namespaces': {}, 'classes': {}}
 
     for root, dirs, files in os.walk(path):
-        if len(files) == 0:
+        if cs_file_count._(files) == 0:
             continue
         address = root.split(os.sep)
         class_name = address.pop()
